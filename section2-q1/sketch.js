3a929cec6f69a637e045d1b8c0af33158c78ba36
yusukeej <65478641+yusukeej@users.noreply.github.com>
2020-10-22 13:23:00 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,5 +2,14 @@
 function setup(){
   for(let i = 0; i < 10; i++){
     // BLANK[1]
-  }
+let x = i * 10 + 10;
+ellipse(50,50,x) ;
+if (i < 5){
+   stroke(0,0,255);
+
+}else {
+  stroke(255,0,0);
+}
+noFill();
+}
 }
