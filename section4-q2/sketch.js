02ce2e8d888bcb62968fcd009006dc3b3ddcfe23
yusukeej <65478641+yusukeej@users.noreply.github.com>
2020-11-20 02:49:05 +0900

    change section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -3,28 +3,41 @@ let x, y, vx, vy;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
-  vx = 8;
-  vy = 8;
-}
+  x = width / 4;
+  y = height / 4;
+  vx = 4;
+  vy = 4;
+  angle  =0;}
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+angle+= 0.1;
+star(x,y,40,angle);
+x+=vx;
+y+=vy;
+
 
   // 端の処理パターン (1) 反対側から出てくる
-  if(x > width){ x = 0; }
-  else if(x < 0){ x = width; }
-  if(y > height){ y = 0; }
-  if(y < 0){ y = height; }
-}
+//   if(x > width){ x = 0; }
+//   else if(x < 0){ x = width; }
+//   if(y > height){ y = 0; }
+//   if(y < 0){ y = height; }
+// }
+
+if(x < 0　　||　x> width){
+  vx=-1*vy;}
+
+  x=constrain(x,0,width);
+    y=constrain(x,0,heigth);
+
 
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+theta  =theta+ angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
