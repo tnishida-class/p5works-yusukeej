cd394f764fa6a41fc3b18b2be69d8e8c4e73ddcb
yusukeej <65478641+yusukeej@users.noreply.github.com>
2020-11-19 14:02:04 +0900

    change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",50,100,color(255,0,0));
 }
 
-function balloon(t){
+function balloon(t,x,y,c){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  fill(c);
+  rect(x,y, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, p+x, h + p+y);
 }
