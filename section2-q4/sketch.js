f4af881e4d88965afbc19af1b5aba0f3156f38d4
yusukeej <65478641+yusukeej@users.noreply.github.com>
2020-11-22 12:56:12 +0900

    change section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -9,13 +9,21 @@ function setup() {
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+if(i%2==0){
+  fill(blue);
+}else{fill(255)}
     rect(0, i * d, width, (i + 1) * d);
   }
 
+fill(255);
+let size=d*5;
+rect(0,0,size,size);
   fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
-
-  fill(255);
+  
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0,0,40,40);
+  rect(60,0,40,40);
+  rect(0,60,40,40);
+  rect(60,60,40,40);
+
 }
