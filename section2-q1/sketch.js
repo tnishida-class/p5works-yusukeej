a63f76fc6e56f20ef0baef271e4c980f3c3a6fac
yusukeej <65478641+yusukeej@users.noreply.github.com>
2020-11-22 13:05:33 +0900

    change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,8 +1,8 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  for(let i = 0; i < 11; i++){
     // BLANK[1]
-let x = i * 10 + 10;
+let x = i *  10;
 ellipse(50,50,x) ;
 if (i < 5){
    stroke(0,0,255);
